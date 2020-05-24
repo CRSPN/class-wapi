1c05a76d725b5ab603b503e77df0dc84d7631849
CRSPN <doeraysme@gmail.com>
2020-05-24 14:21:26 -0600

    added func for task-actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,11 @@ var createTaskEl = function(taskDataObj) {
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" 
+    + taskDataObj.name 
+    + "</h3><span class='task-type'>" 
+    + taskDataObj.type 
+    + "</span>";
     
     listItemEl.appendChild(taskInfoEl);
 
@@ -86,17 +90,39 @@ var createTaskActions = function(taskId) {
 formEl.addEventListener("submit", taskFormHandler);
 
 var taskButtonHandler = function(event) {
-    console.log(event.target);
+    var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) {
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+
+    else if (event.target.matches(".delete-btn")) {
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
 
    
 };
-var deleteTask = function(taskId) {
+var editTask = function(taskId) {
+    
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+};
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" 
+    + taskId 
+    + "']");
     taskSelected.remove();
 };
 pageContentEl.addEventListener("click", taskButtonHandler);
